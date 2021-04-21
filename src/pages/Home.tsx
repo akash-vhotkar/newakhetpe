b0ba1af23f299d288d5e54805a0d987ea276aec3
Shivank Varshney <shivank1703@gmail.com>
2021-04-21 13:23:43 +0530

    toolbar right side icons added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import Logo from "../assets/logo.png";
+import Gold from "../assets/gold.png";
+import Silver from "../assets/silver.png";
+import Friend from "../assets/friend.png";
 import Play from "../assets/play-button.png";
 
 const Home: React.FC = () => {
@@ -8,10 +11,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonToolbar className="ion-toolbar">
           <IonTitle className="ion-title">
-            <nav className="navbar p-0">
-              <a className="navbar-brand" href="#">
-                <img src={Logo} alt="akhetpe logo" className="logo"/>
-              </a>
+            <nav className="navbar px-0">
+              <img src={Logo} alt="akhetpe logo" className="logo"/>
+              <div className="ml-auto">
+              <img src={Gold} alt="akhetpe logo" className="logos"/>
+              <img src={Silver} alt="akhetpe logo" className="logos"/>
+              <img src={Friend} alt="akhetpe logo" className="logos"/>
+              </div>
             </nav>
           </IonTitle>
         </IonToolbar>
